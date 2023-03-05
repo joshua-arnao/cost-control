0b95320db431cfedd74f355b42f05946975e4441
joshua-arnao <joshua.arnao@icloud.com>
2023-03-05 12:53:09 -0500

    format numbers to money

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const BudgetControl = ({ budget }) => {
+  const amountFormat = (amount) => {
+    return amount.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
       <div>
@@ -9,7 +16,15 @@ const BudgetControl = ({ budget }) => {
 
       <div className='contenido-presupuesto'>
         <p>
-          <span>Presupuesto: </span> ${budget}
+          <span>Presupuesto: </span> {amountFormat(budget)}
+        </p>
+
+        <p>
+          <span>Disponible: </span> {amountFormat(0)}
+        </p>
+
+        <p>
+          <span>Gastado: </span> {amountFormat(0)}
         </p>
       </div>
     </div>
